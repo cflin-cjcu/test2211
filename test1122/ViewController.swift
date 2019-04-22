17e1608cded9ce7bf2ccff1514253aca3bd01543
cflin-cjcu <cflin@mail.cjcu.edu.tw>
2019-04-22 11:55:46 +0800

    label & button

--- test1122/ViewController.swift
+++ test1122/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lv_name: UILabel!
+    @IBOutlet weak var bt_submit: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func submit(_ sender: UIButton) {
+        lv_name.text = "AAAA"
+        
+    }
+    
 
 }
 
