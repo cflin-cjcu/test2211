e0c20fd8ff2e9d450465f10bb09e6a3ca7036d25
cflin-cjcu <cflin@mail.cjcu.edu.tw>
2019-04-29 11:50:11 +0800

    three view

--- test1122/ViewController.swift
+++ test1122/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var lv_name: UILabel!
     @IBOutlet weak var bt_submit: UIButton!
     
+    @IBOutlet weak var img_aa: UIImageView!
+    @IBOutlet weak var viewA: UIButton!
+    @IBOutlet weak var viewB: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
